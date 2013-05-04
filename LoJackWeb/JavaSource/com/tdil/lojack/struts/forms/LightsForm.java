3c6a8676b36be931ed13e8d045b891f241b41f2e
subcmd@gmail.com <subcmd@gmail.com@713a6d25-0446-b6cd-5ebb-89ab6377d058>
2013-05-04 17:59:37 +0000

    Se toma la descripcion y el email notif de la base local

--- LoJackWeb/JavaSource/com/tdil/lojack/struts/forms/LightsForm.java
+++ LoJackWeb/JavaSource/com/tdil/lojack/struts/forms/LightsForm.java
@@ -1,15 +1,23 @@
 package com.tdil.lojack.struts.forms;
 
+import java.sql.SQLException;
 import java.util.Collection;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionMapping;
 
+import com.tdil.lojack.daomanager.DAOManager;
 import com.tdil.lojack.gis.LoJackServicesConnector;
+import com.tdil.lojack.gis.model.Alarm;
 import com.tdil.lojack.gis.model.Light;
+import com.tdil.lojack.model.AlarmConf;
+import com.tdil.lojack.model.LightConf;
+import com.tdil.lojack.model.LightConfExample;
 import com.tdil.lojack.utils.WebsiteUser;
+import com.tdil.struts.TransactionalActionWithResult;
 
 public class LightsForm extends ActionForm {
 
@@ -18,6 +26,18 @@ public class LightsForm extends ActionForm {
 	private WebsiteUser user;
 	private Collection<Light> lights;
 	
+	private static final class GetLightConf implements TransactionalActionWithResult {
+		private int userId;
+		public GetLightConf(int userId) {
+			super();
+			this.userId = userId;
+		}
+		public Object executeInTransaction() throws SQLException {
+			LightConfExample example = new LightConfExample();
+			example.createCriteria().andIdwebsiteuserEqualTo(userId);
+			return DAOManager.getLightConfDAO().selectLightConfByExample(example);
+		}
+	}
 		
 	@Override
 	public void reset(ActionMapping mapping, HttpServletRequest request) {
@@ -30,8 +50,28 @@ public class LightsForm extends ActionForm {
 	public void initWith(WebsiteUser user) {
 		setUser(user);
 		setLights(LoJackServicesConnector.getLights(String.valueOf(user.getId())));
+		try {
+			List<LightConf> lightConf = (List<LightConf>)new GetLightConf(user.getModelUser().getId()).executeInTransaction();
+			for (Light light : getLights()) {
+				enhance(light, lightConf);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
+	private void enhance(Light light, List<LightConf> lightConf) {
+		for (LightConf lc : lightConf) {
+			if (lc.getIdentidad().equals(light.getIdEntidad())) {
+				if (lc.getIdluz().equals(light.getIdLuz())) {
+					light.setEmailnotification(lc.getEmailnotification() == null ? false : (lc.getEmailnotification().equals(1)));
+					light.setDescription(lc.getDescription());
+					return;
+				}
+			}
+		}
+	}
 
 	public WebsiteUser getUser() {
 		return user;
