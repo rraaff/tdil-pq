bd78b235f802f731903403412d64d6df58141875
subcmd@gmail.com <subcmd@gmail.com@713a6d25-0446-b6cd-5ebb-89ab6377d058>
2013-05-04 18:45:46 +0000

    Modificacion de AsyncJob

---
+++ LoJackWeb/JavaSource/com/tdil/lojack/gis/model/AsynJobConstants.java
@@ -0,0 +1,14 @@
+package com.tdil.lojack.gis.model;
+
+public class AsynJobConstants {
+
+	// Acciones
+	
+	// Estados
+	public static final int INCORRECTPARAMS = 0;
+	public static final int PENDING = 1;
+	public static final int SENDEDTOGIS = 2; 
+	public static final int OK = 3;
+	public static final int FAILED = 4; 
+	public static final int SENDMESSAGE = 5; 
+}
