cd03eaedae703778f94b482629136f1fb7129f34
thatdayinlondon@gmail.com <thatdayinlondon@gmail.com@713a6d25-0446-b6cd-5ebb-89ab6377d058>
2014-03-27 16:26:09 +0000

    1.02.09 / versión 10 Nueva: Problema con el sincro con Google Play

--- native/LoJackRLAndroid/src/com/tdil/thalamus/android/CheckForUpdateActivity.java
+++ native/LoJackRLAndroid/src/com/tdil/thalamus/android/CheckForUpdateActivity.java
@@ -109,9 +109,10 @@ public class CheckForUpdateActivity extends Activity {
             .setMessage("Hay una actualización disponible.¿Desea actualizarla ahora?")
             .setPositiveButton("Actualizar", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int whichButton) {
-                            /* User clicked OK so do some stuff */
-                            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://search?q=pname:lojackapp"));
-                            startActivity(intent);
+                        /* User clicked OK so do some stuff */
+                        //Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://search?q=pname:lojack.real.life"));
+                    	Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.tdil.lojack.rl"));
+                        startActivity(intent);
                     }
             })
             .setNegativeButton("Salir", new DialogInterface.OnClickListener() {
